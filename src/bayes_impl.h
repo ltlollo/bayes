98c7a4b14a595a7ac2ef6836df8c90d6a54ad7c9
ltlollo <ltlollo.it@gmail.com>
2014-09-26 23:23:22 +0200

    s:mv pred kind, pred was used

--- src/bayes_impl.h
+++ src/bayes_impl.h
@@ -1,49 +1,48 @@
-
 template<typename T>
-void Bayes::biased_train(T prop, const Text& text) {
+void Bayes::biased_train(T kind, const Text& text) {
     auto rnd = std::generate_canonical<double, 1>(gen);
     if (rnd <= 0.2) {
-        train(prop, text);
+        train(kind, text);
     } else {
-        double bias = couriosity(prop)*interesting(text);
+        double bias = couriosity(kind)*interesting(text);
         if (rnd <= 0.2 + bias) {
-            train(prop, text);
+            train(kind, text);
         }
     }
 }
 
 template<typename T>
-void Bayes::aged_train(T prop, const Text& text) {
+void Bayes::aged_train(T kind, const Text& text) {
     if (randfreq(std::exp(-total.sum()/ageing_rate))) {
-        train(prop, text);
+        train(kind, text);
     }
 }
 
 template<typename T>
-void Bayes::biased_aged_train(T prop, const Text& text) {
+void Bayes::biased_aged_train(T kind, const Text& text) {
     auto rnd = std::generate_canonical<double, 1>(gen);
     auto ageing = std::exp(-total.sum()/ageing_rate);
     if (rnd <= ageing) {
-        train(prop, text);
+        train(kind, text);
     } else {
-        double bias = couriosity(prop)*interesting(text);
+        double bias = couriosity(kind)*interesting(text);
         if (rnd <= ageing*std::exp(total.sum()*bias/ageing_rate)) {
-            train(prop, text);
+            train(kind, text);
         }
     }
 }
 
 template<typename T>
-double Bayes::pcond(const Text& text, T prop) const noexcept {
+double Bayes::pcond(const Text& text, T kind) const noexcept {
     double prob {1.0};
     for (const auto& stat : stats) {
         if (!influencing(stat.counts)) {
             break;
         }
         if (text.find(stat.pred) != text.end()) {
-            prob *= psmoothed(stat.counts.get(prop), total.get(prop));
+            prob *= psmoothed(stat.counts.get(kind), total.get(kind));
         } else {
-            prob *= (1.0 - psmoothed(stat.counts.get(prop), total.get(prop)));
+            prob *= (1.0 - psmoothed(stat.counts.get(kind), total.get(kind)));
         }
     }
     return prob;
